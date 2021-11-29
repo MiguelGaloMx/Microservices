18c4017dab41ed57fe16f0ab5a58431c38a36697
MiguelGaloMx <39218015+MiguelGaloMx@users.noreply.github.com>
2021-11-29 11:34:02 -0600

    Add files via upload

---
+++ Servicios.api.Libreria/Controllers/AutorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.api.Libreria.Core.Entities;
+using Servicios.api.Libreria.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servicios.api.Libreria.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AutorController : ControllerBase
+	{
+		private readonly IMongoRepository<AutorEntity> _autorRepository;
+		public AutorController(IMongoRepository<AutorEntity> autorRepository)
+		{
+			_autorRepository = autorRepository;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AutorEntity>>> Get() =>
+			Ok(await _autorRepository.GetAll());
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AutorEntity>> GetById(string Id) =>
+			Ok(await _autorRepository.GetById(Id));
+		
+		[HttpPost]
+		public async Task Post(AutorEntity autorEntity)
+		{
+			await _autorRepository.InsertDocument(autorEntity);
+		}
+		
+		[HttpPut]
+		public async Task Put(AutorEntity autorEntity)
+		{
+			await _autorRepository.UpdateDocument(autorEntity);
+		}
+		
+		[HttpDelete]
+		public async Task Delete(string Id)
+		{
+			await _autorRepository.DeleteById(Id);
+		}
+
+		[HttpPost("pagination")]
+		public async Task<ActionResult<Pagination<AutorEntity>>> PostPagination(Pagination<AutorEntity> pagination)
+		{
+			var result = await _autorRepository.PaginationByFilter(pagination);
+			return Ok(result);
+		}
+	}
+}
---
+++ Servicios.api.Libreria/Controllers/LibroController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicios.api.Libreria.Core.Entities;
+using Servicios.api.Libreria.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Servicios.api.Libreria.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LibroController : ControllerBase
+	{
+		private readonly IMongoRepository<LibroEntity> _repository;
+		public LibroController(IMongoRepository<LibroEntity> repository)
+		{
+			_repository = repository;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<LibroEntity>>> Get() =>
+			Ok(await _repository.GetAll());
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<LibroEntity>> GetById(string Id) =>
+			Ok(await _repository.GetById(Id));
+
+		[HttpPost("pagination")]
+		public async Task<ActionResult<Pagination<LibroEntity>>> PostPagination(Pagination<LibroEntity> pagination) =>
+			Ok(await _repository.PaginationByFilter(pagination));
+
+		[HttpPost]
+		public async Task Post(LibroEntity libro) =>
+			await _repository.InsertDocument(libro);
+
+		[HttpPut]
+		public async Task Put(LibroEntity libro) =>
+			await _repository.UpdateDocument(libro);
+
+		[HttpDelete]
+		public async Task Delete(string Id) =>
+			await _repository.DeleteById(Id);
+	}
+}
